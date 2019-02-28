360c1a6fd09af3999e9d968c9499c35a0cb76e6e
Joel Robert Justiawan <joelwindows7@gmail.com>
2019-02-28 22:28:17 +0700

    AI Concepttual design
    is here now.

---
+++ Assets/Kejar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Kejar : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent me;
+    [SerializeField] private Transform Target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        me = GetComponent<NavMeshAgent>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Target) me.SetDestination(Target.position);
+    }
+}
---
+++ Assets/SQLiteTest.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mono.Data.Sqlite;
+using System.Data;
+using System;
+
+public class SQLiteTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string conn = "URI=file:" + Application.dataPath + "/SQL/PickAndPlaceDatabase.s3db.db"; //Path to database.
+        IDbConnection dbconn;
+        dbconn = (IDbConnection)new SqliteConnection(conn);
+        dbconn.Open(); //Open connection to the database.
+        IDbCommand dbcmd = dbconn.CreateCommand();
+
+
+
+
+
+        string sqlQuery = "SELECT value,name, randomSequence " + "FROM PlaceSequence";
+        dbcmd.CommandText = sqlQuery;
+        IDataReader reader = dbcmd.ExecuteReader();
+
+
+        while (reader.Read())
+        {
+            int value = reader.GetInt32(0);
+            string name = reader.GetString(1);
+            int rand = reader.GetInt32(2);
+
+            Debug.Log("value= " + value + "  name =" + name + "  random =" + rand);
+        }
+
+        reader.Close();
+        reader = null;
+        dbcmd.Dispose();
+        dbcmd = null;
+        dbconn.Close();
+        dbconn = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
