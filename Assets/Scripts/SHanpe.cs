6a6eff8cd8471d2c95d525fc621150412f972115
Joel Robert Justiawan <joelwindows7@gmail.com>
2019-03-08 22:26:02 +0700

    blender tutorial & SHanpe moving
    We learnt blender again and it is so many. just learn what we need instead.
    
    also 3D version of Move character is way more different. we are learning milestone!
    we need to know how to roll based on what camera sees ahead and slide based on what camera sees.
    we need contraining deadzone. from the off the deadzone, it must be zero. We thought constraint. So benefiting many controller to add more velocity likely be removed here. deadzone is min and 1 is max I think. ahh.

--- Assets/Scripts/SHanpe.cs
+++ Assets/Scripts/SHanpe.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class SHanpe : MonoBehaviour
 {
+
+    public ShapeList[] shapeLists;
+    public int shapeIndex = 0;
+
+    public float Torqueing = 0f;
+    public float Forcing = 0f;
+
+    //Deadzoning. Many 3rd party controllers are badly programmed! not all people has luxury!
+    public float DeadzoneUp = 0.01f;
+    public float DeadzoneDown = -0.01f;
+    public float DeadzoneLeft = -0.01f;
+    public float DeadzoneRight = 0.01f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +26,19 @@ public class SHanpe : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+
+        if(Input.GetAxis("Horizontal") >= DeadzoneRight || Input.GetAxis("Horizontal") <= DeadzoneLeft)
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.right * Forcing * Input.GetAxis("Horizontal"));
+            GetComponent<Rigidbody>().AddTorque(Vector3.right * Torqueing * Input.GetAxis("Horizontal"));
+        }
+        if (Input.GetAxis("Vertical") >= DeadzoneUp || Input.GetAxis("Vertical") <= DeadzoneDown)
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.forward * Forcing * Input.GetAxis("Vertical"));
+            GetComponent<Rigidbody>().AddTorque(Vector3.forward * Torqueing * Input.GetAxis("Vertical"));
+        }
+
+        //Debug.Log(Input.GetAxis("Horizontal")); //result = -1, 0, 1, float value
     }
 }
--- Assets/Scripts/ShapeList.cs
+++ Assets/Scripts/ShapeList.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class ShapeList : MonoBehaviour
 {
+
+    public float Torqueing = 0f;
+    public float Forcing = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
