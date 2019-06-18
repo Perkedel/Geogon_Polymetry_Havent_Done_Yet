d5216fa61b591e3ed439642c561d5bd021d6ef88
Joel Robert Justiawan <joelwindows7@gmail.com>
2019-06-18 14:11:39 +0700

    lecturing for last time
    we are going to presentation
    
    also continued the time piece for vilbrid's i smeel cheese but hatkid smeel time piece

---
+++ Assets/Scripts/behaviourpeluru.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class behaviourpeluru : MonoBehaviour
+{
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(Vector3.forward * 1000);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
